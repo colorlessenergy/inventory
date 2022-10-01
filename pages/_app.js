7973ac2e398bdda27be4925af075e0d3209be7cd
colorlessenergy <brianmunozdev@gmail.com>
2022-10-01 10:52:17 -0700

    add a default item

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,21 @@ import '../styles/globals.scss';
 function MyApp({ Component, pageProps }) {
     useEffect(() => {
         if (!localStorage.getItem('ID')) {
-            localStorage.setItem('ID', JSON.stringify(0));
+            localStorage.setItem('ID', JSON.stringify(1));
         }
 
         if (!localStorage.getItem('inventory')) {
-            localStorage.setItem('inventory', JSON.stringify([]));
+            localStorage.setItem(
+                'inventory',
+                JSON.stringify([
+                    {
+                        ID: 0,
+                        emoji: '🍊',
+                        item: 'tangerine',
+                        amount: 1
+                    }
+                ])
+            );
         }
     }, []);
 

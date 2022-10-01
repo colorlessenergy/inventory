bbb59d660bb89ee4550f1541d9b7056958a847e1
colorlessenergy <brianmunozdev@gmail.com>
2022-10-01 10:44:20 -0700

    move delete item functionality into a component

--- components/Item.js
+++ components/Item.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import DeleteItem from './DeleteItem';
 import Emoji from './Item/Emoji';
 import Text from './Item/Text';
 import Amount from './Item/Amount';
@@ -27,14 +28,6 @@ const Item = ({ ID, emoji, item, amount, setInventory }) => {
 
         setInventory(JSON.parse(localStorage.getItem('inventory')));
     };
-    const deleteItem = () => {
-        let inventory = JSON.parse(localStorage.getItem('inventory'));
-        const itemIndex = inventory.findIndex(item => item.ID === ID);
-        inventory.splice(itemIndex, 1);
-        localStorage.setItem('inventory', JSON.stringify(inventory));
-
-        setInventory(JSON.parse(localStorage.getItem('inventory')));
-    };
 
     return (
         <div className="item mb-1">
@@ -46,15 +39,8 @@ const Item = ({ ID, emoji, item, amount, setInventory }) => {
                         done
                     </button>
                 ) : null}
-                <button onClick={deleteItem}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        width="20"
-                        height="20">
-                        <path d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3zm1 2H6v12h12V8zm-9 3h2v6H9v-6zm4 0h2v6h-2v-6zM9 4v2h6V4H9z" />
-                    </svg>
-                </button>
+
+                <DeleteItem ID={ID} setInventory={setInventory} />
             </div>
             <div className="flex-flow">
                 <div className="flex-flow">
